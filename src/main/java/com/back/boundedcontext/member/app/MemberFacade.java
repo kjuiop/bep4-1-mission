1e3deec95da48404b7a797a164bdf5cd2c494164
kjuiop <copying_y@naver.com>
2025-12-23 16:19:18 +0900

    0012

--- src/main/java/com/back/boundedcontext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedcontext/member/app/MemberFacade.java
@@ -3,6 +3,7 @@ package com.back.boundedcontext.member.app;
 import com.back.boundedcontext.member.domain.Member;
 import com.back.global.exception.DomainException;
 import com.back.boundedcontext.member.out.MemberRepository;
+import com.back.global.rsdata.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +27,7 @@ public class MemberFacade {
     }
 
     @Transactional
-    public Member join(String username, String password, String nickname) {
+    public RsData<Member> join(String username, String password, String nickname) {
         findByUsername(username).ifPresent(member -> {
             throw new DomainException("409-1", "이미 존재하는 username 입니다.");
         });
--- src/main/java/com/back/boundedcontext/member/app/MemberJoinUseCase.java
+++ src/main/java/com/back/boundedcontext/member/app/MemberJoinUseCase.java
@@ -3,6 +3,7 @@ package com.back.boundedcontext.member.app;
 import com.back.boundedcontext.member.domain.Member;
 import com.back.boundedcontext.member.out.MemberRepository;
 import com.back.global.exception.DomainException;
+import com.back.global.rsdata.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,11 +19,13 @@ public class MemberJoinUseCase {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Member join(String username, String password, String nickname) {
+    public RsData<Member> join(String username, String password, String nickname) {
         memberRepository.findByUsername(username).ifPresent(member -> {
             throw new DomainException("409-1", "이미 존재하는 username 입니다.");
         });
 
-        return memberRepository.save(new Member(username, password, nickname));
+        Member saved = memberRepository.save(new Member(username, password, nickname));
+
+        return new RsData<>("201-1", "%d번 회원이 생성되었습니다.".formatted(saved.getId()), saved);
     }
 }
